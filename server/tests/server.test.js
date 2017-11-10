83731f68d8bdd8abc1c2a7e86e0a00492bbf5b4a
szyncel <mtteu1994@gmail.com>
2017-11-10 23:54:03 +0100

    GET /todos tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,8 +8,16 @@ const {
     Todo
 } = require('./../models/todo');
 
+const todos = [{
+    text: "first Todo"
+}, {
+    text: "Second Todo"
+}];
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => {
         done();
     })
 })
@@ -32,7 +40,9 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({
+                    text
+                }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -53,7 +63,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e);
@@ -63,3 +73,18 @@ describe('POST /todos', () => {
 
 
 })
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+
+
+          
+    })
+});
