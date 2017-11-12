c9d90c63fc19146471b6636459e9c98907932f26
szyncel <mtteu1994@gmail.com>
2017-11-12 14:45:16 +0100

    set up to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/TodoApp', {
+mongoose.connect('mongodb://szyncel:szynka123@ds257245.mlab.com:57245/szyncel-todo-test' ||'mongodb://localhost/TodoApp', {
     useMongoClient: true
 });
 mongoose.Promise = global.Promise;
--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ var Todo = require('./models/todo').Todo;
 
 
 var app = express()
+var port= process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -64,8 +65,8 @@ app.get('/todos/:id', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log("Started on port 3000");
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {
